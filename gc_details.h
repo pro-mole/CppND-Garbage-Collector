6501d7f868c709615e1ee56be65d4d31b4c4c675
Bruno Guedes <guedesav@brazmogu.com.br>
2019-09-04 00:56:43 -0300

    PtrDetails Implementation

--- gc_details.h
+++ gc_details.h
@@ -20,14 +20,18 @@ array, then arraySize contains its size */
 
     PtrDetails(void)
     {
-        // TODO: Implement PtrDetails
+        refcount = 0;
+        memPtr = nullptr;
+        isArray = false;
+        arraySize = 0;
     }
 };
+
 // Overloading operator== allows two class objects to be compared.
 // This is needed by the STL list class.
 template <class T>
 bool operator==(const PtrDetails<T> &ob1,
                 const PtrDetails<T> &ob2)
 {
-    // TODO: Implement operator==
+    return ob1.memPtr == ob2.memPtr;
 }
